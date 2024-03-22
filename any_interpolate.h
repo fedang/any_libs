8f528a086e5cd37873dce733dc06be6a62248ecf
Federico Angelilli <code@fedang.net>
2024-03-22 10:26:25 +0100

    Add license and sources

--- any_interpolate.h
+++ any_interpolate.h
@@ -238,4 +238,37 @@ double any_cubic_bezier(double x, double a, double b, double c, double d)
 	return a * (t * t * t) + 3 * b * (t * t * x) + 3 * c * (t * x * x) + d * (x * x * x);
 }
 
+// TODO: Add spline and hermite
+
 #endif
+
+// Sources
+// - https://en.wikipedia.org/wiki/Smoothstep
+// - https://www.shadertoy.com/view/ltSfWV
+// - https://www.shadertoy.com/view/ltjcWW
+// - https://www.shadertoy.com/view/MdBfR1
+// - https://www.shadertoy.com/view/ltByWW
+// - http://demofox.org/bezcubic1drational.html
+
+// MIT License
+//
+// Copyright (c) 2024 Federico Angelilli
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
